cecc15e1e0928f1389213c53f04b7baf39edb9fc
luckydragon65 <alias0223@naver.com>
2021-12-29 00:36:21 +0900

    ㅠ

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,8 +1,31 @@
 
-<TextInput bind:value={player1} labelText="player1" placeholder="Enter user name..." />
-<TextInput bind:value={player2} labelText="player2" placeholder="Enter user name..." />
 
-<Select labelText="map" bind:selected={map}>
+
+
+
+<div class="header">
+
+<Select labelText="플레이어1" bind:selected={player1}>
+  <SelectItem value="" text="선택" disabled hidden />
+  {#each allPlayer as mens}
+  <SelectItem value={mens.player}.{mens.univ} text={mens.player} />
+  {/each}
+</Select>
+
+
+
+
+
+<Select labelText="플레이어2" bind:selected={player2}>
+  <SelectItem value="" text="선택" disabled hidden />
+  {#each allPlayer as mens}
+  <SelectItem value={mens.player}.{mens.univ} text={mens.player} />
+  {/each}
+</Select>
+
+
+<Select labelText="맵" bind:selected={map}>
+  <SelectItem value="" text="선택" disabled hidden />
   <SelectItem value="투혼" text="투혼" />
   <SelectItem value="폴리" text="폴리" />
   <SelectItem value="이클" text="이클" />
@@ -10,45 +33,81 @@
 </Select>
 
 
+
+
 <RadioButtonGroup
   orientation="vertical"
   legendText="승 선택"
   bind:selected={w}
 >
-  <RadioButton labelText="player1" value="player1" />
-  <RadioButton labelText="player2" value="player2" />
+
+  <RadioButton labelText="플레이어1" bind:value={player1} />
+  <RadioButton labelText="플레이어2" bind:value={player2}  />
+
 
 </RadioButtonGroup>
 
 
-<button on:click={addMatch}>Add</button>
-
-
-
-
-<DataTable
-size="compact"
-expandable
-headers={[  { key: 'player1', value: 'player1' },  { key: 'player2', value: 'player2' },  { key: 'map', value: 'map' },  { key: 'w', value: 'w' }]}
-rows={allMatchs}
->
-<div slot="expanded-row" let:row>
-  <pre>
-    {JSON.stringify(row, null,1)}
-    
-  </pre>
+<button on:click={addMatch}>전적등록</button>
 </div>
-</DataTable>
 
+<StructuredList condensed>
+  <StructuredListHead>
+    <StructuredListRow head>
+      <StructuredListCell head>일자</StructuredListCell>
+      <StructuredListCell head>플레이어1</StructuredListCell>
+      <StructuredListCell head>맵</StructuredListCell>
+      <StructuredListCell head>플레이어2</StructuredListCell>
+   
+    </StructuredListRow>
+  </StructuredListHead>
+  <StructuredListBody >
+   
+    {#each allMatchs as match}
+    <StructuredListRow >
+      <StructuredListCell >{match.createdAt}</StructuredListCell>
+      {#if match.player1 === match.w}
+      <StructuredListCell >{match.player1} <img src={match.player1univimg} alt="media"/> <span class="win"></span>  </StructuredListCell>
+      <StructuredListCell >{match.map}</StructuredListCell>
+      <StructuredListCell ><img src={match.player2univimg} alt="media"/>  {match.player2} </StructuredListCell>
+      {:else}
+      <StructuredListCell >{match.player1} <img src={match.player1univimg} alt="media"/></StructuredListCell>
+      <StructuredListCell >{match.map}</StructuredListCell>
+      <StructuredListCell > <span class="win"></span> <img src={match.player2univimg} alt="media"/> {match.player2} </StructuredListCell>
+      {/if}
+    </StructuredListRow>
+    {/each}
+
+  </StructuredListBody>
+</StructuredList>
+
+
+<!-- 
+ <DataTable
+ size="tall"
+ expandable
+ headers={[ {key:'createdAt',value:'createdAt'},  { key: 'player1', value: 'player1' },  { key: 'map', value: 'map' },   { key: 'player2', value: 'player2' }  ]}
+ rows={allMatchs}
+ >
+ <div slot="expanded-row" let:row>
+   <pre>
+     {JSON.stringify(row, null,1)}
+     
+   </pre>
+ </div>
+ </DataTable> 
+-->
+
+<div class="header">
 
 
 {#if $authStore.firebaseControlled}
  {#if !$authStore.isLoggedIn}
- <button on:click={login}>login google</button>
+ <button on:click={login}>로그인</button>
 
 
 {:else}
-<button on:click={logout}>logout google</button>
+<button on:click={logout}>로그아웃</button>
 
 
 {/if}
@@ -57,29 +116,27 @@ rows={allMatchs}
 
 
 
+<TextInput bind:value={addplayer} labelText="선수등록" placeholder="선수입력" />
+
+<Select labelText="스타대학" bind:selected={univ}>
+  <SelectItem value="NSU" text="NSU" />
+  <SelectItem value="미다스" text="미다스" />
+  <SelectItem value="마종대" text="마종대" />
+  <SelectItem value="바스포드" text="바스포드" />
+  <SelectItem value="우끼끼즈" text="우끼끼즈" />
+  <SelectItem value="염석대" text="염석대" />
+  <SelectItem value="철기중대" text="철기중대" />
+  <SelectItem value="친박연대" text="친박연대" />
+  <SelectItem value="캄성여대" text="캄성여대" />
+  <SelectItem value="파이스트" text="파이스트" />
+  <SelectItem value="학버드" text="학버드" />
+  <SelectItem value="FA" text="FA" />
+</Select>
+
+<button on:click={createPlayer}>선수등록</button>
 <slot></slot>
 
-
-
-  
-
-
-
-
-
-
-<input type="text" placeholder="Add a task" bind:value={task} />
-
-<button on:click={addTodo}>Add</button>
-
-
-
-
-<svelte:window on:keydown={keyIsPressed} />
-
-
-<svelte:head><link rel="stylesheet" href="https://unpkg.com/carbon-components-svelte@0.50.2/css/white.css" /></svelte:head>
-
+</div>
 <script>
 import {
   auth, 
@@ -95,22 +152,29 @@ import {
     deleteDoc,  
     doc,  
     onSnapshot, 
-    updateDoc } from 'firebase/firestore';
+    updateDoc,
+    query, 
+    where, 
+    orderBy,
+getDocs,
+Firestore,
+
+    } from 'firebase/firestore';
 
  
-
-    
-import { onDestroy, onMount } from 'svelte';
+import { onDestroy, onMount} from 'svelte';
 import authStore from '$lib/authStore';
 import { goto } from '$app/navigation';
 
 
 
-
-
 let allMatchs=[];
 const ref = collection(db,'match')
-onSnapshot(ref,(sn)=>{
+
+const q = query(ref, orderBy("createdAt", 'asc'));
+
+//const query(on)Snapshot = await getDocs(q);
+onSnapshot(q,(sn)=>{
     let matchs =[];
 sn.forEach((doc)=>{
 
@@ -121,29 +185,6 @@ allMatchs = matchs;
 
 })
 
-
-
-let task = "";
-  let error = "";
-  const addTodo = async () => {
-    if (task !== "") {
-      const docRef = await addDoc(collection(db, "todos"), {
-        task: task,
-        isComplete: false,
-        createdAt: new Date(),
-      });
-      error = "";
-    } else {
-      error = "Task is empty";
-    }
-    task = "";
-  };
-
-  const keyIsPressed = (event) => {
-    if (event.key === "Enter") {
-      addTodo();
-    }
-  };
 
 
 const login = async()=>{
@@ -161,13 +202,7 @@ const logout = async()=>{
 
     } catch (e) {
       console.log(e);
-    }
-}
-
-
-
-
-
+    }}
 
 
 
@@ -196,11 +231,11 @@ const sub = authStore.subscribe(async (info) => {
       });
       
      
+
     
     });
 
  
-
 
     import { 
       DataTable,
@@ -208,39 +243,314 @@ const sub = authStore.subscribe(async (info) => {
       RadioButtonGroup, 
       RadioButton, 
       Select, 
-      SelectItem  } from "carbon-components-svelte";
+      SelectItem,
+    StructuredList,
+    StructuredListHead,
+    StructuredListRow,
+    StructuredListCell,
+    StructuredListBody,
+ } from "carbon-components-svelte";
 
     let w ="";
     let player1="";
     let player2="";
     let map = "";
+    let player1univ = "";
+    let player2univ = "";
+  
+    
+ 
+
+    function getUniv(playeruniv){
+      let result ;
+      switch (playeruniv) {
+      
+      case 'NSU':
+      result = "https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/nsu.png?alt=media&token=32d3c9e7-f66d-4a29-9d2b-d1ea60046293";
+         break;
+      case '미다스':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/midas.png?alt=media&token=d6001b53-6eee-4355-b1a6-0bb2aa778f27";
+         break;
+      case '마종대':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/minor.png?alt=media&token=4c512892-a2e1-410c-af48-3544bc3df69f";
+         break;
+      case '바스포드':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/bas.png?alt=media&token=f7f07db6-b724-4b6a-9d64-0574ec741784";
+         break;
+      case '우끼끼즈':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/ukkz.png?alt=media&token=961d87b4-5dd2-42c1-bb58-40763cc7bd03";
+         break;
+      case '염석대':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/ysu.png?alt=media&token=9f1335a4-f4ff-4dfe-bd4e-f3e7130cc1d8";
+         break;
+      case '철기중대':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/cgd.png?alt=media&token=4a299a12-183a-4462-86b2-b59623ec7e56";
+         break;
+      case '친박연대':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/CP.png?alt=media&token=46f5231e-b928-4957-a185-a73f54a53a7e";
+         break;
+      case '캄성여대':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/cs.png?alt=media&token=84c16f35-679d-4ff0-bed5-786a41c15b1a";
+         break;
+      case '파이스트':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/pist.png?alt=media&token=5f21e5e5-8c92-46a6-bc3a-dc3c7cb9d435";
+         break; 
+      case '학버드':
+      result="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/hak.png?alt=media&token=19b14b60-757f-4ce7-96d5-6b58048f33dd";
+         break;
+      default:
+      result="";
+     }
+
+     return result;
+    }
+
+
     let addMatch= async ()=>{
     
     if (player1 !== "" && player2 !== "" && w !== "" && map !=="") {
+     try {
+   
+      player1univ =player1.split('.')[1];
+      let player1univimg = getUniv(player1univ);
+      player2univ =player2.split('.')[1];
+      let player2univimg = getUniv(player2univ);
+      w = w.split('.')[0];
+      player1 = player1.split('.')[0];
+      player2 = player2.split('.')[0];
+     
       const docRef = await addDoc(collection(db, "match"), {
         player1:player1,
+        player1univimg:player1univimg,
         player2:player2,
+        player2univimg:player2univimg,
         w:w,
         map:map,
-        createdAt: new Date(),
+        createdAt:  new Date().toLocaleString()
+        
       });
-      error = "";
-    } else {
-      error = "Task is empty";
-    }
-    player1 = "";
-    player2 = "";
-    map="";
-    w="";
+
+      player1univ="";
+      player2univ="";
+      player1="";
+      player2="";
+      player1univimg="";
+      player2univimg="";
+      w="";
+      map="";
+     } catch (error) {
+       console.error(error)
+     } 
 
 
+
+     
+    } 
+   
   };
  
 
+
+
+  let addplayer ="";
+  let univ ="";
+  let univimg="";
+
+
+  
+
+  let createPlayer= async ()=>{
+    
+    if (addplayer !== "" ) {
+     try {
+     switch (univ) {
+     case 'NSU':
+         univ ="NSU";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/nsu.png?alt=media&token=32d3c9e7-f66d-4a29-9d2b-d1ea60046293";
+         break;
+     case '미다스':
+         univ ="미다스";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/midas.png?alt=media&token=d6001b53-6eee-4355-b1a6-0bb2aa778f27";
+         break;
+     case '마종대':
+         univ ="마종대";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/minor.png?alt=media&token=4c512892-a2e1-410c-af48-3544bc3df69f";
+         break;
+   
+     case '바스포드':
+         univ ="바스포드";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/bas.png?alt=media&token=f7f07db6-b724-4b6a-9d64-0574ec741784";
+         break;
+     case '우끼끼즈':
+         univ ="우끼끼즈";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/ukkz.png?alt=media&token=961d87b4-5dd2-42c1-bb58-40763cc7bd03";
+         break;
+     case '염석대':
+         univ ="염석대";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/ysu.png?alt=media&token=9f1335a4-f4ff-4dfe-bd4e-f3e7130cc1d8";
+         break;
+     case '철기중대':
+          univ ="철기중대";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/cgd.png?alt=media&token=4a299a12-183a-4462-86b2-b59623ec7e56";
+         break;
+    case '친박연대':
+         univ ="친박연대";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/CP.png?alt=media&token=46f5231e-b928-4957-a185-a73f54a53a7e";
+         break;
+    case '캄성여대':
+          univ ="캄성여대";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/cs.png?alt=media&token=84c16f35-679d-4ff0-bed5-786a41c15b1a";
+         break;
+    case '파이스트':
+          univ ="파이스트";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/pist.png?alt=media&token=5f21e5e5-8c92-46a6-bc3a-dc3c7cb9d435";
+         break; 
+    case '학버드':
+          univ ="학버드";
+         univimg="https://firebasestorage.googleapis.com/v0/b/blog-61ad4.appspot.com/o/hak.png?alt=media&token=19b14b60-757f-4ce7-96d5-6b58048f33dd";
+         break;
+    default:
+          univ ="FA";
+         univimg="";
+     }
+
+      const docRef = await addDoc(collection(db, "player"), {
+        player:addplayer,
+        univ :univ,
+        univimg :univimg,
+        createdAt: new Date().toLocaleString()
+
+      });
+     } catch (error) {
+       console.error(error)
+     } 
+     
+    } else {
+     
+    }
+    addplayer ="";
+    univ ="";
+    univimg="";
+   
+    
+
+  };
+
+
+  let allPlayer=[];
+const playerRef = collection(db,'player')
+const playerQ = query(playerRef, orderBy("createdAt", 'asc'));
+//const query(on)Snapshot = await getDocs(q);
+onSnapshot(playerQ,(sn)=>{
+    let players =[];
+ sn.forEach((doc)=>{
+
+ let men ={ ...doc.data(),id:doc.id};
+ players=[men, ...players];
+ })
+
+
+ allPlayer = players;
+
+})
 </script>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<style>
+
+<style global>
+
+body{
+
+    width: 60%;
+    margin-left: auto;
+    margin-right: auto;
+    padding-top: 8%;
+width:850px;
+background-color: #F2F0F9;
+}
+  img{  
+    height:30px;
+    width:30px;
+    vertical-align: middle;
+    }
+    .win{      
+    display: inherit;
+    width: 10px;
+    height: 10px;
+    text-align: center;
+    background-color: #ff0000;
+    border-radius: 5px;
+    -webkit-border-radius: 5px;
+    vertical-align: center;
+    }
+select {
+  border: 0;
+  margin: 10px;
+}
+.bx--radio-button-group{
+  border: 0;
+  margin: 10px;
+}
+
+.bx--form-item{
+  display: inline-block;
+  vertical-align: middle;
+}
+
+.bx--structured-list-row--header-row{
+  text-align: center;
+  border-bottom: 1px solid #C6C2DE;
+  border-top: 1px solid #C6C2DE;
+  background-color: #F2F0F9;
+  color: #6D5BD0;
+}
+.bx--structured-list-th{
+  width: 200px;
+  font-weight: bolder;
+  font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  display: inline-block;
+ 
+}
+.bx--structured-list-td{
+  width: 206px;
+  display:inline-block;
+  text-align: center;
+  font-weight: bold;
+}
+
+button{
+  display: inline-block;
+  background-color: #6D5BD0;
+  box-shadow: 1px 1px 2px 2px #C6C2DE;
+  border-radius: 5px;
+  border:none;
+  color: white;
+  width:80px;
+  height: 40px;
+  
+}
+#svelte{
+  border: 0px solid #C6C2DE;
+  box-shadow: 1px 1px 2px 2px #C6C2DE;
+  background-color: white;
+  border-radius: 5px;
+}
+.bx--structured-list-tbody{
+  text-align: center;
+  justify-content:center; 
+}
+ 
 
 
+.bx--structured-list-row:hover{
+  background-color:#dfdff7;
+  background-image:linear-gradient(to right, rgba(255,255,255,0) 0%, rgba(255,255,255,0) 40%, rgba(255,255,255,.7) 100%);
+  background-repeat:no-repeat;
+  background-size: 200% 100%; 
+  transition:background-size 1s, background-color 1s;
+}
+.header{
+  text-align-last: justify;
+  justify-content:center; 
+  
+  margin: 10px;
+}
 </style>
-
